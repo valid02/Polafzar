12f32385a8f540d7cf9c632c8232ab2c26365d5a
Valid Rabiee <validrabieejs@gmail.com>
2024-01-27 05:33:00 +0330

    adding AddCategory component

---
+++ src/components/Category/AddCategory.js
@@ -0,0 +1,40 @@
+import { faAngleLeft, faXmark } from '@fortawesome/free-solid-svg-icons';
+import IconBtn from '../UI/IconBtn';
+import classes from './AddCategory.module.css';
+import { useState } from "react";
+import AddButton from '../UI/AddButton';
+
+const AddCategory = ({ goBackHandler , newCategoryHandler, closeModal  }) => {
+  const [enteredCategory, setEnteredCategory] = useState('');
+  const handleBackButtonClick = () => {
+    goBackHandler(false);
+  }
+
+  const addCategoryHandler = event => {
+    event.preventDefault();
+    newCategoryHandler(enteredCategory);
+  }
+
+  return (
+    <>
+      <header className={classes['add-category__header']}>
+        <IconBtn onClick={closeModal} icon={faXmark} />
+        <h2 className={classes['add-category__title']}>افزودن دسته بندی</h2>
+        <IconBtn onClick={handleBackButtonClick} icon={faAngleLeft} />
+      </header>
+      <div className={classes['add-category__content']}>
+        <form onSubmit={addCategoryHandler}>
+          <div className={classes['add-category__control']}>
+            <label>نام دسته بندی</label>
+            <input type="text" onChange={e => setEnteredCategory(e.target.value)} />
+          </div>
+          <div className={classes['add-category__control']}>
+            <AddButton onClick={addCategoryHandler} text="افزودن" />
+          </div>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default AddCategory;
