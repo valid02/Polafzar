b80c5f448763e3384456c1133e6bfb21ed1f259b
Valid Rabiee <validrabieejs@gmail.com>
2024-01-28 18:57:10 +0330

    Refactor CategoryItem component to handle edit button dynamically

--- src/components/Category/CategoryItem.js
+++ src/components/Category/CategoryItem.js
@@ -3,21 +3,12 @@ import classes from './CategoryItem.module.css';
 import { faPen } from '@fortawesome/free-solid-svg-icons';
 
 const CategoryItem = ({ icon, title, isEditable }) => {
-  let item = (
-    <li className={classes['category-item']}>
-      <FontAwesomeIcon icon={icon} /> {title}
+  return (
+    <li className={isEditable ? classes['category-item-editable'] : classes['category-item']}>
+      <span> <FontAwesomeIcon icon={icon} /> {title} </span>
+      {isEditable && <button className={classes['category-edit-btn']} title='ویرایش'><FontAwesomeIcon icon={faPen} /></button>}
     </li>
   );
-  if (isEditable) {
-    item = (
-      <li className={classes['category-item-editable']}>
-        <span> <FontAwesomeIcon icon={icon} /> {title} </span>
-        <button className={classes['category-edit-btn']} title='ویرایش'><FontAwesomeIcon icon={faPen} /></button>
-      </li>
-    );
-  }
-
-  return item;
 };
 
 export default CategoryItem;
