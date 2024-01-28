e1eac200efe62ccd884d71d7608a91ddf405e745
Valid Rabiee <validrabieejs@gmail.com>
2024-01-28 18:49:23 +0330

    added edit button to CategoryItem component

--- src/components/Category/CategoryItem.js
+++ src/components/Category/CategoryItem.js
@@ -1,12 +1,23 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classes from './CategoryItem.module.css';
+import { faPen } from '@fortawesome/free-solid-svg-icons';
 
-const CategoryItem = ({ icon, title }) => {
-  return (
+const CategoryItem = ({ icon, title, isEditable }) => {
+  let item = (
     <li className={classes['category-item']}>
       <FontAwesomeIcon icon={icon} /> {title}
     </li>
   );
+  if (isEditable) {
+    item = (
+      <li className={classes['category-item-editable']}>
+        <span> <FontAwesomeIcon icon={icon} /> {title} </span>
+        <button className={classes['category-edit-btn']} title='ویرایش'><FontAwesomeIcon icon={faPen} /></button>
+      </li>
+    );
+  }
+
+  return item;
 };
 
 export default CategoryItem;
